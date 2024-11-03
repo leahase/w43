a86de126fc9191630ef47c0dc59864f398d4e05e
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-11-03 17:27:13 +0100

    button

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function guessNumber(){
-    const numberResult = Math.floor(Math.random() * 10) + 1; //så att det blir random mellan 1 och 10
+    const numberResult = (Math.floor(Math.random() * 10) + 1).toString(); //så att det blir random mellan 1 och 10
     let guess;
 
-    while (guess !== numberResult){ //om gissning inte är resultatet av random
+    while (guess !== numberResult) { //om gissning inte är resultatet av random
         guess = prompt('guess a number between 1 and 10;');
 
     if (guess === numberResult) {
@@ -16,4 +16,3 @@ function guessNumber(){
     }
 }
 }
-guessNumber();
